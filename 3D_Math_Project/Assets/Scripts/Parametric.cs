26666dbd6dcf230c9beef23196a83f7736137e9a
WittlyEli <162608185+EliksonRod@users.noreply.github.com>
2026-03-17 14:03:07 -0400

    Finished parametric assignment

--- 3D_Math_Project/Assets/Scripts/Parametric.cs
+++ 3D_Math_Project/Assets/Scripts/Parametric.cs
@@ -1,27 +1,23 @@
+using System.Collections;
+using System.Security.Cryptography;
 using Unity.VisualScripting;
 using UnityEngine;
-using System.Collections;
+using UnityEngine.InputSystem;
 
 public class Parametric : MonoBehaviour
 {
     [SerializeField]
     GameObject pointObject;
 
-    int resolution = 10;
-
-    public float coeffA = 1f;
-    public float coeffB = 2f;
-
-    public float exponentA = 1f;
-    public float exponentB = 1f;
-
+    public int resolution = 10;
     public bool DrawGraph = true;
+    public bool UseZ = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    public float r0 = 1f;
+    public float r1 = 1f;
 
-    }
+    public float d0 = 45f;
+    public float d1 = 45f;
 
     // Update is called once per frame
     void Update()
@@ -34,20 +30,29 @@ public class Parametric : MonoBehaviour
         if (!DrawGraph) return;
 
         var scale = Vector3.one / resolution;
+        GameObject graph;
         float t = Time.time;
 
-        //
-        float x = coeffA * Mathf.Sin(Mathf.Pow(t, exponentA));
-        float y = coeffB * Mathf.Cos(Mathf.Pow(t, exponentB));
-        GameObject graph = Instantiate(pointObject, new Vector3(x, y, 0), Quaternion.identity, transform);
+        float x = (r0 - r1) * Mathf.Cos(t) + d0 * Mathf.Cos(((r0 - r1) / r1) * t);
+        float y = (r0 - r1) * Mathf.Sin(t) - d0 * Mathf.Sin(((r0 - r1) / r1) * t);
+        float z = (r0 - r1) * Mathf.Sin(t) - d0 * Mathf.Sin(((r0 - r1) / r1) * t);
 
-        /**
-         * 
-         * do another parametric curve!!
-         *
-         **/
+        float a = (r0 + r1) * Mathf.Cos(t) - d1 * Mathf.Cos(((r0 + r1) / r1) * t);
+        float b = (r0 + r1) * Mathf.Sin(t) - d1 * Mathf.Sin(((r0 + r1) / r1) * t);
+        float c = (r0 - r1) * Mathf.Sin(t) + d1 * Mathf.Sin(((r0 - r1) / r1) * t);
 
-        //scales your graph points down according to resolution
-        graph.transform.localScale = scale;
+        //Instantiate points  
+        if (UseZ)
+        {
+            graph = Instantiate(pointObject, new Vector3(x, y, z), Quaternion.identity, transform);
+            graph = Instantiate(pointObject, new Vector3(a, b, c), Quaternion.identity, transform);
+        }
+        else
+        {
+            graph = Instantiate(pointObject, new Vector3(x, y, 0), Quaternion.identity, transform);
+            graph = Instantiate(pointObject, new Vector3(a, b, 0), Quaternion.identity, transform);
+        }
+            //scales your graph points down according to resolution
+            graph.transform.localScale = scale;
     }
 }
