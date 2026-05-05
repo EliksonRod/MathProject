6d050fcf7b3a67dea2df22c0ff911d331e0318aa
WittlyEli <162608185+EliksonRod@users.noreply.github.com>
2026-05-05 19:30:45 -0400

    Starting reflection assignment

--- 3D_Math_Project/Assets/Scripts/CrossProduct.cs
+++ 3D_Math_Project/Assets/Scripts/CrossProduct.cs
@@ -11,25 +11,35 @@ public class CrossProductExample : MonoBehaviour
         {
             Vector3 hitPos = hitInfo.point;
             Vector3 up = hitInfo.normal;
-            Vector3 NormalOpp = -hitInfo.normal;
             Vector3 right = Vector3.Cross(up, laserDir);
             Vector3 forward = Vector3.Cross(right, up);
 
-            //laser
+            Vector3 NormalOpposite = -hitInfo.normal;
+
+            Vector3 Reflect = Vector3.Cross(NormalOpposite, hitPos);
+
+            //Start laser
             Gizmos.color = Color.magenta;
             Gizmos.DrawLine(laserStart, hitPos);
+            //Gizmos.DrawRay(hitPos, -laserStart);
 
-            //draw "up" basis, ie the normal
+            //Opposite normal
             Gizmos.color = Color.green;
-            Gizmos.DrawRay(hitPos, NormalOpp);
+            Gizmos.DrawRay(hitPos, up);
+            Gizmos.DrawRay(hitPos, NormalOpposite);
+
+            //Reflected vector
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(hitPos, Reflect);
+            //Gizmos.DrawRay(hitPos, Reflect);
 
             //draw "right" basis
-            Gizmos.color = Color.red;
-            Gizmos.DrawRay(hitPos, right);
+            //Gizmos.color = Color.red;
+            //Gizmos.DrawRay(hitPos, right);
 
             //draw "forward" basis
-            Gizmos.color = Color.cyan;
-            Gizmos.DrawRay(hitPos, forward);
+            //Gizmos.color = Color.cyan;
+            //Gizmos.DrawRay(hitPos, forward);
 
         }
         else
