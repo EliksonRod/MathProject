17f0317b2c59f00f843b10ac64881fb145e2572c
WittlyEli <162608185+EliksonRod@users.noreply.github.com>
2026-05-07 17:44:52 -0400

    Working on reflections

--- 3D_Math_Project/Assets/Scripts/CrossProduct.cs
+++ 3D_Math_Project/Assets/Scripts/CrossProduct.cs
@@ -1,7 +1,9 @@
+
 using UnityEngine;
 
 public class CrossProductExample : MonoBehaviour
 {
+    public int maxBounces = 5;
     void OnDrawGizmos()
     {
         Vector3 laserStart = transform.position;
@@ -10,37 +12,30 @@ public class CrossProductExample : MonoBehaviour
         if (Physics.Raycast(laserStart, laserDir, out RaycastHit hitInfo))
         {
             Vector3 hitPos = hitInfo.point;
-            Vector3 up = hitInfo.normal;
-            Vector3 right = Vector3.Cross(up, laserDir);
-            Vector3 forward = Vector3.Cross(right, up);
+            Vector3 hitNormal = hitInfo.normal.normalized;
+            Vector3 NormalOpposite = -hitInfo.normal.normalized;
+            //Vector3 continuedLaser
 
-            Vector3 NormalOpposite = -hitInfo.normal;
+            float Reflect = Vector3.Dot(laserDir, hitNormal);
+            Vector3 ReflectVector = laserDir - 2 * Reflect * hitNormal;
 
-            Vector3 Reflect = Vector3.Cross(NormalOpposite, hitPos);
+            Vector3 continuedLaser = hitPos + laserDir;
+
 
             //Start laser
             Gizmos.color = Color.magenta;
             Gizmos.DrawLine(laserStart, hitPos);
-            //Gizmos.DrawRay(hitPos, -laserStart);
+            //Gizmos.DrawRay(hitPos, continuedLaser);
 
             //Opposite normal
             Gizmos.color = Color.green;
-            Gizmos.DrawRay(hitPos, up);
-            Gizmos.DrawRay(hitPos, NormalOpposite);
+            Gizmos.DrawRay(hitPos, hitNormal);
+            //Gizmos.DrawRay(hitPos, NormalOpposite);
 
             //Reflected vector
             Gizmos.color = Color.red;
-            Gizmos.DrawRay(hitPos, Reflect);
+            Gizmos.DrawRay(hitPos, ReflectVector);
             //Gizmos.DrawRay(hitPos, Reflect);
-
-            //draw "right" basis
-            //Gizmos.color = Color.red;
-            //Gizmos.DrawRay(hitPos, right);
-
-            //draw "forward" basis
-            //Gizmos.color = Color.cyan;
-            //Gizmos.DrawRay(hitPos, forward);
-
         }
         else
         {
@@ -48,7 +43,6 @@ public class CrossProductExample : MonoBehaviour
             Gizmos.color = Color.white;
             Gizmos.DrawRay(laserStart, laserDir);
         }
-
     }
 }
 
